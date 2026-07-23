cb4754483f77b501b443340e363e7262b36d69f5
MarekK <marekkplw@gmail.com>
2026-07-23 12:02:24 +0100

    feat: add symbolic manifest package

---
+++ qviraex/symbolic/__init__.py
@@ -0,0 +1,17 @@
+"""Symbolic, non-supernatural protocol artifacts for MirrorME."""
+
+from .vireax_sigil import (
+    DEFAULT_VIREAX_INTENT,
+    SigilElement,
+    VireaxSigilManifest,
+    build_vireax_sigil_manifest,
+    condense_intent,
+)
+
+__all__ = [
+    "DEFAULT_VIREAX_INTENT",
+    "SigilElement",
+    "VireaxSigilManifest",
+    "build_vireax_sigil_manifest",
+    "condense_intent",
+]
